5ff970289960d04c81785ccc6d8867e34c2d7296
GustavoH1709 <gustavoh1709@hotmail.com>
2025-11-13 20:52:34 -0300

    a

--- PocSbCredito/PocSbCredito.Domain/Contracts/Recebivel/Handlers/GetRecebivelHandler.cs
+++ PocSbCredito/PocSbCredito.Domain/Contracts/Recebivel/Handlers/GetRecebivelHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using PocSbCredito.Domain.Contracts.Recebivel.Requests;
 using PocSbCredito.Domain.Contracts.Recebivel.Results;
 using PocSbCredito.Infrastructure.Database;
+using PocSbCredito.Shared.Extensions;
 using PocSbCredito.Shared.Models;
 
 namespace PocSbCredito.Domain.Contracts.Recebivel.Handlers
@@ -13,19 +14,31 @@ namespace PocSbCredito.Domain.Contracts.Recebivel.Handlers
         {
             ObjectResponse<List<GetRecebivelResult>> result = new([]);
 
-            var query = await (from a in db.TB_Recebivel
-                               join b in db.TB_Empresa on a.EmpresaId equals b.Id
-                               select new GetRecebivelResult()
-                               {
-                                   DataEmissao = a.DataEmissao.ToString("dd/MM/yyyy"),
-                                   DataVencimento = a.DataVencimento.ToString("dd/MM/yyyy"),
-                                   NumeroDocumento = a.NumeroDocumento,
-                                   RecebivelId = a.Id,
-                                   StatusRecebivel = a.StatusRecebivel,
-                                   NomeEmpresa = b.Nome
-                               }).ToListAsync(cancellationToken);
+            var query = (from a in db.TB_Recebivel
+                         join b in db.TB_Empresa on a.EmpresaId equals b.Id
+                         select new
+                         {
+                             DataEmissao = a.DataEmissao.ToString("dd/MM/yyyy"),
+                             DataVencimento = a.DataVencimento.ToString("dd/MM/yyyy"),
+                             NumeroDocumento = a.NumeroDocumento,
+                             RecebivelId = a.Id,
+                             StatusRecebivel = a.StatusRecebivel,
+                             NomeEmpresa = b.Nome,
+                             EmpresaId = b.Id
 
-            result.Value = query;
+                         })
+                         .Filter(!string.IsNullOrWhiteSpace(request.StatusRecebivel), item => item.StatusRecebivel == request.StatusRecebivel)
+                         .Filter(request.EmpresaId is not null && request.FundoId != Guid.Empty, item => item.EmpresaId == request.EmpresaId);
+
+            result.Value = await query.Select(x => new GetRecebivelResult()
+            {
+                DataEmissao = x.DataEmissao,
+                DataVencimento = x.DataVencimento,
+                NumeroDocumento = x.NumeroDocumento,
+                RecebivelId = x.RecebivelId,
+                StatusRecebivel = x.StatusRecebivel,
+                NomeEmpresa = x.NomeEmpresa
+            }).ToListAsync(cancellationToken);
 
             return result;
         }
--- PocSbCredito/PocSbCredito.Domain/Contracts/Recebivel/Requests/GetRecebivelRequest.cs
+++ PocSbCredito/PocSbCredito.Domain/Contracts/Recebivel/Requests/GetRecebivelRequest.cs
@@ -6,5 +6,8 @@ namespace PocSbCredito.Domain.Contracts.Recebivel.Requests
 {
     public class GetRecebivelRequest : IRequest<ObjectResponse<List<GetRecebivelResult>>>
     {
+        public Guid? EmpresaId { get; set; }
+        public Guid? FundoId { get; set; }
+        public string? StatusRecebivel { get; set; }
     }
 }
